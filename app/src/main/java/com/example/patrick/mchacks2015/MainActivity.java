750bfa27b4c27ca6f61c14d8d976fc0b8499d7df
PatzHum <patrick.hum@hotmail.com>
2015-02-21 23:17:01 -0500

    done

--- app/src/main/java/com/example/patrick/mchacks2015/MainActivity.java
+++ app/src/main/java/com/example/patrick/mchacks2015/MainActivity.java
@@ -8,20 +8,31 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
     int values[] = new int[4];
-    String ops[] = new String[3];
+    String opers[] = new String[3];
     Button rNum[] = new Button[4];
     Button rOp[] = new Button [3];
     Button nPlace[] = new Button[4];
+    String svaltoget;
+    TextView valToGet;
+    int score;
+
+    private LinkedList<Character> ops = new LinkedList<> (Arrays.asList('^','/','*','-','+'));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //blah blah blah
         super.onCreate(savedInstanceState);
+        score = 0;
+
         setContentView(R.layout.activity_main);
 
         rNum[0] = (Button) findViewById(R.id.rNum1);
@@ -38,9 +49,12 @@ public class MainActivity extends ActionBarActivity {
         nPlace[2] = (Button) findViewById(R.id.nVal3);
         nPlace[3] = (Button) findViewById(R.id.nVal4);
 
+        valToGet = (TextView) findViewById(R.id.strToGetTo);
+
         //gen rand #s
         setRands(10);
 
+        
     }
 
     private void setRands(int lim){
@@ -48,6 +62,7 @@ public class MainActivity extends ActionBarActivity {
         Random r = new Random();
         int rndNum;
         for(int i=0; i<4; i++){
+            nPlace[i].setText("");
             rndNum = r.nextInt(lim)+1;
             rNum[i].setText(String.valueOf(rndNum));
             values[i] = rndNum;
@@ -56,8 +71,14 @@ public class MainActivity extends ActionBarActivity {
         for (int i=0; i<3; i++){
             rndChar = intToChar(r.nextInt(4));
             rOp[i].setText(rndChar);
-            ops[i] = rndChar;
+            opers[i] = rndChar;
         }
+        String calcStr = String.valueOf(values[0])+opers[0]+String.valueOf(values[1])+opers[1]+String.valueOf(values[2])+opers[2]+String.valueOf(values[3]);
+        svaltoget = edmas(calcStr).get(0);
+        if (Double.parseDouble(svaltoget) % 1.0 != 0.0) {
+            setRands(10);
+        }
+        valToGet.setText(svaltoget);
 
     }
 
@@ -78,30 +99,76 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void buttonClick(View view){
-        int idNum = view.getId();
-        int tag = Integer.valueOf(
-                view.getTag(
-                        idNum
-                ).toString());
+        int tag=0;
+        switch (view.getId()){
+            case R.id.rNum1:
+                tag = 0;
+                break;
+            case R.id.rNum2:
+                tag = 1;
+                break;
+            case R.id.rNum3:
+                tag = 2;
+                break;
+            case R.id.rNum4:
+                tag = 3;
+                break;
+        }
 
-        Button button = (Button) findViewById(idNum);
-
-        button.setText("");
 
         for (int i=0; i<4;i++){
-            if (nPlace[i].getText()!=""){
-                nPlace[i].setText(values[i]);
+            if (nPlace[i].getText().equals("") && !rNum[tag].getText().equals("")){
+                nPlace[i].setText(String.valueOf(values[tag]));
+                rNum[tag].setText("");
+                break;
             }
         }
 
-        
 
+        if(!nPlace[0].getText().equals("") && !nPlace[1].getText().equals("") && !nPlace[2].getText().equals("") && !nPlace[3].getText().equals("")) {
+            if (svaltoget.equals(edmas(
+                    nPlace[0].getText().toString() +
+                            opers[0] +
+                            nPlace[1].getText().toString() +
+                            opers[1] +
+                            nPlace[2].getText().toString() +
+                            opers[2] +
+                            nPlace[3].getText().toString()).get(0))) {
+                score++;
+                TextView sc = (TextView) findViewById(R.id.textCountDown);
+                sc.setText(String.valueOf(score));
+                setRands(10);
+            }
+        }
+    }
+    public void returnVal(View view){
+        int tag=0;
+        switch (view.getId()){
+            case R.id.nVal1:
+                tag = 0;
+                break;
+            case R.id.nVal2:
+                tag = 1;
+                break;
+            case R.id.nVal3:
+                tag = 2;
+                break;
+            case R.id.nVal4:
+                tag = 3;
+                break;
+        }
+        for (int i=0; i<4;i++){
+            if (String.valueOf(values[i]).equals(nPlace[tag].getText().toString()) && rNum[i].getText().equals("")){
+                rNum[i].setText(String.valueOf(values[i]));
+                nPlace[tag].setText("");
+                break;
+            }
+        }
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-
 
         return true;
     }
@@ -119,5 +186,77 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private List<String> split_by_char_arr(String s, List<Character> a){
+        List<String> split_eq;
+        String d = "";
+        for (int i=0; i<a.size();i++){
+            d += delim(a.get(i))+"|";
+        }
+        d = d.substring(0,d.length()-1);
+        split_eq = new LinkedList<>(Arrays.asList(s.split(d)));
+        return split_eq;
+    }
+
+    private List<String> edmas(String eq){
+        List <String> e = split_by_char_arr(eq,ops);
+
+        e.removeAll(Collections.singleton(""));
+
+        for (int i=0; i<e.size(); i++){
+            if (ops.contains(Character.valueOf(e.get(i).charAt(0))) && e.get(i).length()==1 && e.get(i+1).equals("-")){
+                e.set(i+1,"-"+e.get(i+2));
+                e.remove(i+2);
+            }
+        }
+        if (e.get(0).equals("-")){
+            e.set(0,"-"+e.get(1));
+            e.remove(1);
+        }
+        for (int i=0; i<ops.size();i++){
+            e = solve(ops.get(i),e);
+        }
+        return e;
+    }
+    private String delim(char ch){
+        //returns properly formatted delimiters for string splitting
+        return "(?=[\\\\" + ch + "])|(?<=[\\\\"+ch+"])";
+    }
+    private List<String> solve(char op, List<String> eq){
+        int i = 0;
+        while(eq.contains(String.valueOf(op))){
+            String j;
+            j = eq.get(i);
+            if(j.equals(String.valueOf(op))){
+                eq.set(i-1, String.valueOf(operator(op,Double.parseDouble(eq.get(i-1)),Double.parseDouble(eq.get(i+1)))));
+                eq.remove(i);
+                eq.remove(i);
+                i-=2;
+            }
+            i++;
+        }
+        return eq;
+    }
+
+    private double operator(char op, double n1, double n2){
+        if (op == '+'){
+            return(n1 + n2);
+        }
+        if (op == '-'){
+            return(n1 - n2);
+        }
+        if (op == '*'){
+            return(n1 * n2);
+        }
+        if (op == '/'){
+            return(n1 / n2);
+        }
+        if (op == '^'){
+            return(Math.pow(n1,n2));
+        }
+        else{
+            return -1;
+        }
     }
 }
